92af8433cc4809c99a5e6ce9ea24b4723212a9fa
Max Melentiev <melentievm@gmail.com>
2015-09-09 18:51:58 +0300

    Railtie initializer

--- lib/rails_stuff/railtie.rb
+++ lib/rails_stuff/railtie.rb
@@ -1,6 +1,54 @@
 require 'rails/railtie'
 
 module RailsStuff
+  MODULES = {
+    nullify_blank_attrs:  :model,
+    random_uniq_attr:     :model,
+    statusable:           :model,
+    resources_controller: [
+      :controller,
+      -> { ResourcesController.kaminari! if defined?(::Kaminari) },
+    ],
+    sort_scope: -> { defined?(::HasScope) && :controller },
+  }
+
+  class << self
+    # Set it to array of modules to load.
+    #
+    #     # config/initializers/rails_stuff.rb
+    #     RailsStuff.load_modules = [:statusable, :sort_scope]
+    attr_accessor :load_modules
+    # Override default base classes for models & controllers.
+    attr_writer :base_controller, :base_model
+
+    def base_controller
+      @base_controller || ActionController::Base
+    end
+
+    def base_model
+      @base_model || ActiveRecord::Base
+    end
+
+    # Extends base controller and model classes with modules.
+    # By default uses all modules. Use load_modules= to override this list.
+    def setup_modules!
+      modules_to_load = load_modules || MODULES.keys
+      MODULES.slice(*modules_to_load).each do |m, (type, init)|
+        m = const_get m.to_s.camelize
+        case type.respond_to?(:call) ? type.call : type
+        when :controller
+          RailsStuff.base_controller.extend m
+        when :model
+          RailsStuff.base_model.extend m
+        end
+        init.try!(:call)
+      end
+    end
+  end
+
   class Railtie < Rails::Railtie
+    initializer :rails_stuff_setup_modules, after: :load_config_initializers do
+      RailsStuff.setup_modules!
+    end
   end
 end
