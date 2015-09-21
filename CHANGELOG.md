5339a00992ecd3ccfb782feebaf7a301389e1047
Max Melentiev <melentievm@gmail.com>
2015-09-21 16:44:59 +0300

    v0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.0
+
+- PluginManager & media queries.
+
 ## 0.2.0
 
 - Bypass block to `respond_with`.
--- lib/rails_stuff/version.rb
+++ lib/rails_stuff/version.rb
@@ -5,7 +5,7 @@ module RailsStuff
 
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 2
+    MINOR = 3
     TINY  = 0
     PRE   = nil
 
