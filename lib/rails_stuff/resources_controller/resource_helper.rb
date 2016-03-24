bbda74336756b23f4ddfe92974b88517d16068f9
Max Melentiev <melentievm@gmail.com>
2016-03-24 14:21:34 +0300

    ResourceHelper generates enquirer method

--- lib/rails_stuff/resources_controller/resource_helper.rb
+++ lib/rails_stuff/resources_controller/resource_helper.rb
@@ -15,15 +15,21 @@ module RailsStuff
       # - `class` - class name, default to `resource_name.classify`
       # - `param` - param name, default to `resource_name.foreign_key`
       def resource_helper(resource_name, **options)
-        helper_method resource_name
+        helper_method resource_name, :"#{resource_name}?"
         resource_name = resource_name.to_s
+        class_name = options[:class] || resource_name.classify
+        param_key = options[:param] || resource_name.foreign_key
 
         class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          protected
+
           def #{resource_name}
-            @#{resource_name} ||= #{options[:class] || resource_name.classify}.
-              find params[:#{options[:param] || resource_name.foreign_key}]
+            @#{resource_name} ||= #{class_name}.find(params[:#{param_key}])
           end
-          protected :#{resource_name}
+
+          def #{resource_name}?
+            params.key?(:#{param_key})
+          end
         RUBY
       end
     end
--- spec/rails_stuff/resources_controller/resource_helper_spec.rb
+++ spec/rails_stuff/resources_controller/resource_helper_spec.rb
@@ -5,19 +5,20 @@ RSpec.describe RailsStuff::ResourcesController::ResourceHelper do
   end
   let(:params) { {user_id: 1, secret_id: 2, admin_user_id: 3} }
   let(:controller) do
-    klass.new.tap do |x|
-      allow(x).to receive(:params) { ActionController::Parameters.new(params) }
-    end
+    params = ActionController::Parameters.new(self.params)
+    klass.new.tap { |x| allow(x).to receive(:params) { params } }
   end
 
   describe '#resource_helper' do
     subject { ->(*args) { klass.resource_helper(*args) } }
 
-    def assert_generated_method(method, class_name, param, options = {}) #
-      expect(klass).to receive(:helper_method).with(method)
+    def assert_generated_method(method, class_name, param, **options)
+      expected_methods = [method, :"#{method}?"]
+      expect(klass).to receive(:helper_method).with(*expected_methods)
       expect { klass.resource_helper method, options }.
-        to change { klass.protected_instance_methods }.by [method]
+        to change { klass.protected_instance_methods }.by expected_methods
       assert_finder_method(method, class_name, param)
+      assert_enquirer_method(method, param)
     end
 
     def assert_finder_method(method, class_name, param)
@@ -25,6 +26,11 @@ RSpec.describe RailsStuff::ResourcesController::ResourceHelper do
       stub_const(class_name.to_s, model)
       expect(model).to receive(:find).with(params[param]) { :found_user }
       expect(controller.send(method)).to eq :found_user
+    end
+
+    def assert_enquirer_method(method, param)
+      expect(controller.params).to receive(:key?).with(param) { :key_result }
+      expect(controller.send("#{method}?")).to eq :key_result
     end
 
     it 'defines resource accessor and calls helper_method' do
